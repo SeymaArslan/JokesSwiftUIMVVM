fb353287116a400d85800cff136d2a7bfbdf197d
Şeyma Arslan <seymarslannn@gmail.com>
2023-10-31 16:27:40 +0300

    Add alamofire, get data in alamofire and update contentView

---
+++ JokesSwiftUIMVVM/JokesData.swift
@@ -0,0 +1,8 @@
+//
+//  JokesData.swift
+//  JokesSwiftUIMVVM
+//
+//  Created by Seyma on 31.10.2023.
+//
+
+import Foundation
---
+++ JokesSwiftUIMVVM/JokesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  JokesViewModel.swift
+//  JokesSwiftUIMVVM
+//
+//  Created by Seyma on 31.10.2023.
+//
+
+import Foundation
