2d3f35a1dce0cda99c134cc6836829c84e893329
Şeyma Arslan <seymarslannn@gmail.com>
2023-10-31 14:16:16 +0300

    Initial Commit

---
+++ JokesSwiftUIMVVM/JokesSwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesSwiftUIMVVMApp.swift
+//  JokesSwiftUIMVVM
+//
+//  Created by Seyma on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesSwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
